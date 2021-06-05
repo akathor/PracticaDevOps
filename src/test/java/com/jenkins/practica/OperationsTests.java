c5e4ee7b7fe75b29f8e537a400d10e8800ac7af7
devops <devops@devops-VirtualBox>
2021-06-05 17:35:51 +0200

    Tests unitarios listos

--- src/test/java/com/jenkins/practica/OperationsTests.java
+++ src/test/java/com/jenkins/practica/OperationsTests.java
@@ -13,13 +13,20 @@ public class OperationsTests {
 	/**
 	 * Checks if the operation getRandomDogImage returns a .jpg
 	 */
+	private DogsOperations dogops;
+	private String img;
+	private ArrayList breeds;
+	
 	@Test
 	public void testRandom() {
 		// Instantiate DogsOperations class
+		dogops = new DogsOperations();
 		
 		// Call getRandomDogImage operation and store the result on String
+		img = dogops.getRandomDogImage();
 		
 		// Assert true if the result string ends with '.jpg'
+		assertEquals(img.endsWith(".jpg"),true);
 	}
 	
 	/**
@@ -28,9 +35,12 @@ public class OperationsTests {
 	@Test
 	public void breedsList() {
 		// Instantiate DogsOperations class
+		dogops = new DogsOperations();
 		
 		// Call getBreedList operation and store the result on ArrayList
+		breeds = dogops.getBreedList();
 		
 		// Assert true if the result ArrayList has size of more than 0
+		assertEquals(breeds.size()>=1,true);
 	}
 }
